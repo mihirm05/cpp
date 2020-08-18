adcb1a648696ed7db508b70860305822942798d5
Mihir Mulye <mihirmulye95@gmail.com>
2020-08-18 11:47:18 +0200

    implementing while without any predetermined number of iterations

---
+++ sentinel control/sentinelcontrol.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string> 
+
+using namespace std;
+
+class Sentinel{
+    // useful in those cases in which the number of iterations are not beforehand 
+    public:
+    int ageCalculator(){
+        int age;
+        int ageTotal;
+        int numberOfPeople;
+
+        cout<<"Enter first person's age or -1 to quit"<<endl;
+        cin>>age;
+
+        while(age != -1){
+            ageTotal = ageTotal + age;
+            numberOfPeople++;
+
+            cout<<"Enter next person's age or -1 to quit"<<endl;
+            cin>>age;
+            
+        }
+        cout<<"Number of people entered: "<<numberOfPeople<<endl;
+        cout<<"Average age: "<<float(ageTotal)/float(numberOfPeople)<<endl;
+        }
+};
+
+int main(){
+    Sentinel s;
+    s.ageCalculator();
+    return 0;
+}
