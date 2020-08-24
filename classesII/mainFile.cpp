855399684d813490624a49ca77eec55baf52c876
Mihir Mulye <mihirmulye95@gmail.com>
2020-08-24 17:46:58 +0200

    implementing arrow selection member operator

--- classesII/mainFile.cpp
+++ classesII/mainFile.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 
 int main(){
+    // creating object of class Printer
     Print p;
+    // creating pointer of class Pointer
+    Print *pPointer = &p;
+
+    // using object to access Printer class function 
     p.write();
+
+    // using arrow member selection selection operator to access Printer class function
+    pPointer->write();
 }
